c21b86f0cb44b9487dfecb94549c3140fde7cb5e
kkhw1000 <31675695+kkhw1000@users.noreply.github.com>
2017-09-08 19:16:00 +0900

    Add files via upload

---
+++ 1st.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+printf("My student id number is 2017301024\n");
+printf("My name is Kim Han Ul\n");
+printf("My Mobile phone number is 010-2042-1093\n");
+printf("My Github address is github.com/kkhw1000\n");
+
+
+return 0;
+}
